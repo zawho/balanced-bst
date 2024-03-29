168b129424c7b487dd44467dc7763b160b92e169
zawho <zthooper2@gmail.com>
2024-03-29 09:10:56 +0800

    Add merge sort funcs and module

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import Tree from './tree';
 import printTree from './print';
 
-const testArr = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+const testArr = [1, 7, 4, 23, 8, 9, 4, 3, 5, 7, 9, 67, 6345, 324];
 const testTree = new Tree(testArr);
 
-printTree(testTree.root);
+// printTree(testTree.root);
--- src/tree.js
+++ src/tree.js
@@ -1,23 +1,22 @@
+import mergeSort from './merge-sort';
 import Node from './tree-node';
 
 class Tree {
 
     constructor(arr) {
-       this.root = this.buildTree(arr, 0, arr.length - 1);
+       this.root = this.buildTree(mergeSort(arr), 0, arr.length - 1);
     }
 
     buildTree(arr, start, end) {
-
         if (start > end) {
             return null;
         }
 
         const mid = Math.floor((start + end) / 2);
         const treeNode = new Node(arr[mid]);
-        
+
         treeNode.left = this.buildTree(arr, start, mid - 1);
         treeNode.right = this.buildTree(arr, mid + 1, end);
-    
         return treeNode;
     }
 
